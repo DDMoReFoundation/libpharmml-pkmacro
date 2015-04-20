0e5b0fb6ade70174c5dcb9ab6df4b1b58fc17930
Florent Yvon <florent@ebi.ac.uk>
2015-04-20 10:31:30 +0100

    toString() update and javadoc

--- src/eu/ddmore/libpharmml/pkmacro/translation/Input.java
+++ src/eu/ddmore/libpharmml/pkmacro/translation/Input.java
@@ -20,25 +20,45 @@ public class Input {
 		this.target = target;
 	}
 
+	/**
+	 * Gets the number of this input within the list it comes from.
+	 * @return An integer starting from 1 to *.
+	 */
 	public int getNumber() {
 		return number;
 	}
 
+	/**
+	 * Gets the type of input, either IV or ORAL.
+	 * @return An {@link InputType} item.
+	 */
 	public InputType getType() {
 		return type;
 	}
 
+	/**
+	 * Gets the type of administration.
+	 * @return The type of administration as a {@link Scalar}.
+	 */
 	public Scalar getAdm() {
 		return adm;
 	}
 
+	/**
+	 * Gets the target variable of the input.
+	 * @return The target as a {@link DerivativeVariable}.
+	 */
 	public DerivativeVariable getTarget() {
 		return target;
 	}
 	
+	/**
+	 * Returns a string representation of this input with the following format:<br>
+	 * Input[&lt;n>] &lt;Type>, adm=&lt;i>, target=&lt;variable>
+	 */
 	@Override
 	public String toString(){
-		return "Input["+number+"] "+type+", adm="+adm+", target="+target;
+		return "Input["+number+"] "+type+", adm="+adm+", target="+target.getSymbId();
 	}
 
 }
